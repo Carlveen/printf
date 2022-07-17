a431dfb33af4ca0ed09c3f3bbd4d28c8a687d25b
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-07-17 04:30:47 -0700

    corrections

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main(void)
+{
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	int len;
+	len = _printf("String:[%s]\n", "I am a string !");
+	int len2;
+	len2 = printf("String:[%s]\n", "I am a string !");
+
+	printf("[%d][%d]", len, len2);
+	_printf("[%d][%d]", len, len2);
+
+	return (0);
+}
