8166b7e317998d97461d4d910e6475b0c868544a
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-07-17 04:07:15 -0700

    putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,37 +1,14 @@
 #include "main.h"
 
 /**
- * _puts - prints a string with newline
- * @str: the string to print
+ * _putchar - a function that prints out a character
+ * @c: the character to be printed out
  *
- * Return:( str-a)
+ * Return: 1
  */
-int _puts(char *str)
+
+int _putchar(char c)
 {
-	char *a = str;/*declaration of variables*/
-
-	while (*str)
-		_putchar(*str++);
-	return (str - a);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1
- */
-int _putchar(int c)
-{
-	static int i;
-	static char buf[OUTPUT_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	write(1, &c, 1);
 	return (1);
 }
--- main.h
+++ main.h
@@ -1,124 +1,29 @@
-#ifndef _PRINTF_H_
-#define _PRINTF_H_
-
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define OUTPUT_BUF_SIZE 1024
-#define BUF_FLUSH -1
-
-#define FIELD_BUF_SIZE 50
-
-#define NULL_STRING "(null)"
-
-#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
+#include <unistd.h>
+#include <limits.h>
 
 /**
- * struct parameters - parameters struct
- *
- * @unsign: flag if unsigned value
- *
- * @plus_flag: on if plus_flag specified
- * @space_flag: on if hashtag_flag specified
- * @hashtag_flag: on if _flag specified
- * @zero_flag: on if _flag specified
- * @minus_flag: on if _flag specified
- *
- * @width: field width specified
- * @precision: field precision specified
- *
- * @h_modifier: on if h_modifier is specified
- * @l_modifier: on if l_modifier is specified
- *
+ * struct string_format - the structure of the string format
+ * @argument: a char pointer to arguments
+ * @function: pointer to function
  */
 
-typedef struct parameters
+typedef struct string_format
 {
-	unsigned int unsign		: 1;
-	unsigned int plus_flag		: 1;
-	unsigned int space_flag		: 1;
-	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag		: 1;
-	unsigned int minus_flag		: 1;
+	char *argument;
+	int (*function)(va_list);
+} string_format;
 
-	unsigned int width;
-	unsigned int precision;
-
-	unsigned int h_modifier		: 1;
-	unsigned int l_modifier		: 1;
-} params_t;
-
-/**
- * struct specifier - Struct token
- *
- * @specifier: format token
- * @f: The function associated
- */
-
-typedef struct specifier
-{
-	char *specifier;
-	int (*f)(va_list, params_t *);
-} specifier_t;
-
-/* _put.c module */
-int _puts(char *str);
-int _putchar(int c);
-
-/* print_functions.c module */
-int print_char(va_list ap, params_t *params);
-int print_int(va_list ap, params_t *params);
-int print_string(va_list ap, params_t *params);
-int print_percent(va_list ap, params_t *params);
-int print_S(va_list ap, params_t *params);
-
-/* number.c module */
-char *convert(long int num, int base, int flags, params_t *params);
-int print_unsigned(va_list ap, params_t *params);
-int print_address(va_list ap, params_t *params);
-
-/* specifier.c module */
-int (*get_specifier(char *s))(va_list ap, params_t *params);
-int get_print_func(char *s, va_list ap, params_t *params);
-int get_flag(char *s, params_t *params);
-int get_modifier(char *s, params_t *params);
-char *get_width(char *s, params_t *params, va_list ap);
-
-/* convert_number.c module */
-int print_hex(va_list ap, params_t *params);
-int print_HEX(va_list ap, params_t *params);
-int print_binary(va_list ap, params_t *params);
-int print_octal(va_list ap, params_t *params);
-
-/* simple_printers.c module */
-int print_from_to(char *start, char *stop, char *except);
-int print_rev(va_list ap, params_t *params);
-int print_rot13(va_list ap, params_t *params);
-
-/* print_number.c module */
-int _isdigit(int c);
-int _strlen(char *s);
-int print_number(char *str, params_t *params);
-int print_number_right_shift(char *str, params_t *params);
-int print_number_left_shift(char *str, params_t *params);
-
-/* params.c module */
-void init_params(params_t *params, va_list ap);
-
-/* string_fields.c modoule */
-char *get_precision(char *p, params_t *params, va_list ap);
-
-/* _prinf.c module */
+int _putchar(char c);
 int _printf(const char *format, ...);
-/* extra prototypes*/
-void format_b(va_list valist, char *buffer, int *index);
-int print_buffer(char *buf, unsigned int nbf);
-insigned; int hand_buffer(char *buf, char c, unsigned int ibf);
-#endif /*_MAIN_H_*/
+int (*parser(const char *argument1, int argument2))(va_list);
+int print_character(va_list list);
+int print_string(va_list list);
+int print_number(va_list list);
+
+#endif
